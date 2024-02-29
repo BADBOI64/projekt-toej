0f7ee1dbdd2a67d148bb3c7e9ba588a16f358836
BADBOI64 <emilsonne8@gmail.com>
2024-02-29 19:02:57 +0100

    forsøg på burgen

--- burgermenu.js
+++ burgermenu.js
@@ -1,11 +1,12 @@
-var menuToggle = document.querySelector("#menu-toggle");
-var activeElements = document.querySelectorAll(".active-element");
-var toggledMenu = menuToggle.addEventListener("click", function(){
-     // forEach is not supported in IE11
-  // activeElements.forEach(function(e){
-  //     e.classList.toggle("active");
-  // });
-     for(var activated = 0; activated < activeElements.length; activated++){
-          activeElements[activated].classList.toggle("active");
-     }
-})
+const bars = document.querySelector(".fa-bars-staggered");
+const mobile = document.querySelector(".header-mobile");
+
+
+
+
+
+
+
+
+
+
