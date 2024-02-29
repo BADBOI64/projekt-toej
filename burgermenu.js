08c07c3b23d53ff3c5cb9fbbbf3d1f9ec82f75bc
BADBOI64 <emilsonne8@gmail.com>
2024-02-29 22:38:42 +0100

    responsivt påbegyndt

--- burgermenu.js
+++ burgermenu.js
@@ -1,6 +1,9 @@
 const bars = document.querySelector(".fa-bars-staggered");
 const mobile = document.querySelector(".header-mobile");
 
+bars.addEventListener("click", () => {
+    mobile.classList.toggle("active");
+});
 
 
 
