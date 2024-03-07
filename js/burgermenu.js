a41034e328e22e73d97ab5b61313f8d748a04636
SorenSpark <tobias5704@gmail.com>
2024-03-07 15:21:50 +0100

    seo optimering af filnavne, samt mere responsivt arbejde

---
+++ js/burgermenu.js
@@ -0,0 +1,15 @@
+const bars = document.querySelector(".fa-bars-staggered");
+const mobile = document.querySelector(".header-mobile");
+
+bars.addEventListener("click", () => {
+    mobile.classList.toggle("active");
+});
+
+
+
+
+
+
+
+
+
